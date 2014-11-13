98f41147ca68e50bd56b5252f20114d5a1a0d306
TakesxiSximada <takesxi.sximada@gmail.com>
2014-11-13 23:10:58 +0900

    modified tests

--- src/sandstorm/utils.py
+++ src/sandstorm/utils.py
@@ -4,13 +4,13 @@ import inspect
 
 
 def get_caller_module():
-    dotted = module = path, dirpath = None
+    dotted = module = path = dirpath = None
     frame = inspect.currentframe().f_back.f_back
     dotted = frame.f_globals['__name__']
     try:
         module = __import__(dotted, globals={}, locals={}, fromlist=True)
-    except ImportError:
-        pass
+    except ImportError:  # pragma: no cover
+        assert False, 'can not import module: {}: {}'.format(dotted, frame)
     else:
         path = module.__file__
         dirpath = os.path.dirname(path)
