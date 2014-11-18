d283f7a79b4866836b6c16dba06b3b709f57e108
TakesxiSximada <takesxi.sximada@gmail.com>
2014-11-19 00:42:31 +0900

    add view config

--- src/sandstorm/middlewares/core.py
+++ src/sandstorm/middlewares/core.py
@@ -1,6 +1,27 @@
 # -*- coding: utf-8 -*-
 
 
+class ViewConfig(object):
+    def __init__(self, *middleware_classes):
+        self.middleware_classes
+
+    def __call__(self, *args, **kwds):
+        middlewares = [klass(*args, **kwds) for klass in self.middleware_classes]
+
+        def _deco(func):
+            def _wrap(obj):
+                for middleware in middlewares:
+                    middleware.setup(obj)
+                rc = func(obj)
+
+                for middleware in middlewares:
+                    middleware.teardown(obj, rc)
+
+                return rc
+            return _wrap
+        return _deco
+
+
 def view_config(middlewares, *args, **kwds):
     _middlewares = middlewares
 
