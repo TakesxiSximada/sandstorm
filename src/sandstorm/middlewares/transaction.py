5aa86b437387eda240232aa02183b39adf78bc1b
TakesxiSximada <takesxi.sximada@gmail.com>
2014-11-20 04:55:45 +0900

    :golf: of TransactionMiddleware code

--- src/sandstorm/middlewares/transaction.py
+++ src/sandstorm/middlewares/transaction.py
@@ -4,15 +4,13 @@ from .core import Middleware
 
 
 class TransactionMiddleware(Middleware):
-    def setup(self, handler):
-        pass
-
     def run(self, func, handler, middlewares):
         try:
             self.setup(handler)
             self._response = self.process(func, handler, middlewares)
         except Exception:
             transaction.abort()
+            raise
         else:
             transaction.commit()
         finally:
