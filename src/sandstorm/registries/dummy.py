e46217029a84c137f24e185eb7a8a1ba904aa60a
TakesxiSximada <takesxi.sximada@gmail.com>
2014-11-24 17:58:13 +0900

    Change the Dummy in complexity

--- src/sandstorm/registries/dummy.py
+++ src/sandstorm/registries/dummy.py
@@ -1,6 +1,20 @@
 # -*- coding: utf-8 -*-
-import zope.interface
+from zope.interface import (
+    Interface,
+    Attribute,
+    implementer,
+    )
 
 
-class IDummyInterface(zope.interface.Interface):
-    pass
+class IDummyInterface(Interface):
+    attribute = Attribute(u'')  # cannot verify
+
+    def func(arg1, arg2):
+        pass
+
+
+@implementer(IDummyInterface)
+class DummyImpl(object):
+
+    def func(self, arg1, arg2):
+        pass
