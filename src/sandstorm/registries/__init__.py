aab74098b2dcea5ef63554f125d814afb428dcbc
TakesxiSximada <takesxi.sximada@gmail.com>
2014-11-24 17:58:57 +0900

    Fixed to register the DummyImpl in registry with includeme, In order to test the BaseRegistryManager

--- src/sandstorm/registries/__init__.py
+++ src/sandstorm/registries/__init__.py
@@ -1,1 +1,8 @@
 # -*- coding: utf-8 -*-
+from .managers import BaseRegistryManager
+from .dummy import DummyImpl
+
+
+def includeme(config):
+    manager = BaseRegistryManager()
+    manager.register('', DummyImpl)
