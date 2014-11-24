8bf77dc52ce3157856b7a76b89ae6c8e85acc89e
TakesxiSximada <takesxi.sximada@gmail.com>
2014-11-24 18:01:42 +0900

    Fixed registry.interfaces in the correct state
    - was wrong the way method of interface definition
    - Add to verify IRegistryManager

--- src/sandstorm/registries/interfaces.py
+++ src/sandstorm/registries/interfaces.py
@@ -1,39 +1,45 @@
 # -*- coding: utf-8 -*-
-import zope.interface
-import zope.interface.adapter
+from zope.interface import (
+    Interface,
+    Attribute,
+    )
 
 
-class IRegistryManager(zope.interface.Interface):
+class IRegistryManager(Interface):
     """レジストリを管理するためのクラス
     zope.interfaceの不足している機能をこのクラスで提供する
     """
-    required = zope.interface.Attribute(u'要求するInterfaceのリスト')
-    provided = zope.interface.Attribute(u'提供するInterface')
-    registry_factory = zope.interface.Attribute(u'使用するRegistryのファクトリ')
+    required = Attribute(u'要求するInterfaceのリスト')
+    provided = Attribute(u'提供するInterface')
+    registry_factory = Attribute(u'使用するRegistryを返すcallable object')
 
-    def implemented_by(self, impl):
+    def verify(impl):
+        """implがinterfaceを提供しているかどうかを確認する
+        """
+
+    def implemented_by(impl):
         """implがinterfaceを提供しているかどうかを確認する
         提供していればTrue、していなければFalseを返す。
         """
         return True or False
 
-    def names(self):
+    def names():
         """管理しているレジストリに登録されている名前の一覧のリストを返す
         """
         return list()
 
-    def register(self, name, impl, update=False):
+    def register(name, impl, update=False):
         """管理しているレジストリに実装を登録する
         """
         pass
 
-    def lookup(self, name):
+    def lookup(name):
         """nameから登録されている実装を取得する
         無い場合はNoneを返す
         """
         return object or None
 
-    def create(self, name, *args, **kwds):
+    def create(name, *args, **kwds):
         """nameから登録されている実装を用いてそれをインスタンス化する
         無い場合はNoneを返す
         """
